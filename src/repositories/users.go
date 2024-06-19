109f3e2f3ed1522f7ad9a855e6cec7724781f8bc
Icaro Campos <icarocampos97@gmail.com>
2024-06-19 16:57:25 -0300

    refactor: Update users repository Create method to use prepared statements for better performance

--- src/repositories/users.go
+++ src/repositories/users.go
@@ -14,6 +14,24 @@ func NewUsersRepository(db *sql.DB) *Users {
 	return &Users{db}
 }
 
-func (u *Users) Create(user models.User) (uint64, error) {
-	return 0, nil
+func (repository Users) Create(user models.User) (uint64, error) {
+	statements, err := repository.db.Prepare(
+		"insert into users (name. nick, email, pass) values(?, ?, ?, ?)",
+	)
+	if err != nil {
+		return 0, err
+	}
+	defer statements.Close()
+
+	result, err := statements.Exec(user.Name, user.Nick, user.Email, user.Pass)
+	if err != nil {
+		return 0, err
+	}
+	lastIDInserted, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(lastIDInserted), nil
+
 }
